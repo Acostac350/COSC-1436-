479cad85d4a30caeff7282d0e39f5e61055f6af9
Ivan Acosta Carrasco <ivan.acostacarrasco@my.tccd.edu>
2025-08-28 21:34:44 -0500

    Testing Class 8/28

---
+++ classwork/PartsOfLanguage/PartsOfLanguage/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Keywords are in blue - have a special meaning to language
+/* This is
+a multiline 
+comment
+*/
+/* This is
+* a multiline
+*/
+// This is also
+// a multiline
+//comment
+void main()
+{
+   //4 + 5 
+    std::cout << "Hello World";
+}
