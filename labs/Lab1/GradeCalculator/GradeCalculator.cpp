4fe83e3f2b7ec39bf99131e54198b64813c0bcfd
Ivan Acosta Carrasco <ivan.acostacarrasco@my.tccd.edu>
2025-09-23 17:39:04 -0500

    Almost finished, I was able to complete the decimal points, just need to verify any requirements in the syntax.

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -7,20 +7,19 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 int main()
 {
     std::cout << "Lab 1" << std::endl; 
     std::cout << "Ivan Acosta Carrasco #1869290" << std::endl; 
-    std::cout << "COSC 1436 Fall 2025" << std::endl;
-    std::cout << "\n";
+    std::cout << "COSC 1436 Fall 2025" << std::endl << std::endl;
     
-    // Used \n because I wanted a space between my credentials and the project
-
     // std::cout << std::fixed << std::setprecision2); USE FOR LATER
     std::string firstName, surName;
     std::cout << "Please enter your name: "; 
     std::cin >> firstName;
     std::getline(std::cin, surName);
+    
     double lab1, lab2, lab3, lab4;
     
     std::cout << "Enter Lab 1: "; 
@@ -59,24 +58,22 @@ int main()
     std::cout << "Lab 2 = " << lab2 << std::endl; 
     std::cout << "Lab 3 = " << lab3 << std::endl; 
     std::cout << "Lab 4 = " << lab4 << std::endl; 
+    std::cout << "Your Lab Average is = " << labAverage << "%" << std::endl << std::endl;
     
-    std::cout << "Your Lab Average is = " << labAverage << std::endl << std::endl;
-    
-    
-    std::cout << firstName << surName << ", Your Exam grades are: " << std::endl << std::endl;
-    
+    std::cout << firstName << surName << ", Your Exam grades are: " << std::endl;
     std::cout << "Exam 1 = " << exam1 << std::endl; 
     std::cout << "Exam 2 = " << exam2 << std::endl;
     std::cout << "Exam 3 = " << exam3 << std::endl; 
-    std::cout << "Your Exam Average is = " << examAverage << std::endl << std::endl;
+    std::cout << "Your Exam Average is = " << examAverage << "%" << std::endl << std::endl;
    
-    std::cout << firstName << surName << ", Your class grade is listed as followed: " <<  std::endl << std::endl;
+    std::cout << firstName << surName << ", Your class grade is listed as followed: " <<  std::endl;
     
-    std::cout << "Lab Average (65%) = " << labAverage << " %" << std::endl;
-    std::cout << "Exam Average (20%) = " << examAverage << " %" << std::endl;
-    std::cout << "Participation (5%) = " << participation << " %" << std::endl;
-    std::cout << "Final Exam is (10%) = " << finalExam << " %" << std::endl;
-    std::cout << "Class Average = " << classAverage << " %" << std::endl;
+    std::cout << "Lab Average (65%) = " << labAverage << "%" << std::endl;
+    std::cout << "Exam Average (20%) = " << examAverage << "%" << std::endl;
+    std::cout << "Participation (5%) = " << participation << "%" << std::endl;
+    std::cout << "Final Exam is (10%) = " << finalExam << "%" << std::endl;
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "Class Average = " << classAverage << "%" << std::endl;
 
   
 }
